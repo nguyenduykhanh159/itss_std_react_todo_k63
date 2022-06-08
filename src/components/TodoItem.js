e0d408f304eacf6fc90c57bfab7ca4be4c3bd21e
khanh.nguyen <khanh.nguyen@eastgate-software.com>
2022-06-09 00:46:31 +0700

    commit final

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
-
-/* 
-  【TodoItemコンポーネント】
-　・Todoアイテムを表示する
-　・チェックボックスにチェックが入っているか管理する
-　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
-*/
-function TodoItem({item}) {
-  const [checked, setChecked] = useState(false);
-
-  const handleClick = () => {
-    setChecked(!checked);
+function TodoItem({ item, onCheck }) {
+  const handleChange = () => {
+    onCheck(item);
   }
-  
   return (
     <label className="panel-block">
-      <input type="checkbox" onClick = {handleClick} />
-      <p className={checked ? "has-text-grey-light" : ""}>{item.text}</p>
+      <input
+        type="checkbox"
+        checked={item.done}
+        onChange={handleChange}
+      />
+      <span className={item.done ? 'has-text-grey-light' : ''}>
+        {item.text}
+      </span>
     </label>
   );
 }
